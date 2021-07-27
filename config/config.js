6d0857f642cb36efe6a6c081480aa10b33e8b94a
Alfredo <thefurok@gmail.com>
2021-07-27 16:41:35 -0500

    created a folder that contain all the application configuration

---
+++ config/config.js
@@ -0,0 +1,17 @@
+/**
+ * Archivo de configuracion general
+ * para las variables de entorno
+ */
+
+require('dotenv').config();
+
+const config = {
+  port: process.env.PORT || 3000,
+  cors: process.env.CORS,
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD,
+  dbHost: process.env.DB_HOST,
+  dbName: process.env.DB_NAME,
+};
+
+module.exports = { config };
