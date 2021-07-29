34802bf1eb188bb4f0ca9c69a03046946c1cd934
Alfredo <thefurok@gmail.com>
2021-07-28 19:26:03 -0500

    created the basic structure of the app

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 
 const app = express();
+const { config } = require('./config/config');
+const userApi = require('./routes/user-routes');
+const universityApi = require('./routes/university-routes');
+// Usamos el middleware de express que nos permite leer datos en formato Json, body-parser
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello word');
-});
+// la aplicacionde express que gestiona las rutas
+userApi(app);
+universityApi(app);
 
-app.listen(3000, () => {
-  console.log('todo corriendo');
-});
-// contraseña mongodb: xkUd2TtnHk4bKZdc
+app.listen(config.port, () =>
+  console.log(`Example app listening on port http://localhost:${config.port}`)
+);
